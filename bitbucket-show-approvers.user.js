ae720688a4b229ba7c87d0d5c28e302dd83a8e71
DBX12 <dbx12@dbx12.de>
2021-02-24 18:02:54 +0100

    Version bump

--- bitbucket-show-approvers.user.js
+++ bitbucket-show-approvers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Show commit approvers on bitbucket
 // @namespace    http://dbx12.de/
-// @version      0.1
+// @version      0.2
 // @description  Show commit approvers
 // @author       dbx12
 // @match        https://bitbucket.org/*/*/pull-requests/*
